866736bee74a9e95a5e5d59846854b0d03ba433a
dunky-star <dukygeoffrey39@gmail.com>
2022-09-04 17:26:14 +0300

    Spring Data JPA with Hibernate ORM

---
+++ spring-data-jpa-dunky/src/main/java/com/dunky/springboot/SpringDataJpaDunkyApplication.java
@@ -0,0 +1,13 @@
+package com.dunky.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaDunkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaDunkyApplication.class, args);
+	}
+
+}
