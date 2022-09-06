43c6a624dfd3ce8c29f125fd3cdddfd1e58e657a
dunky-star <dukygeoffrey39@gmail.com>
2022-09-06 14:28:24 +0300

    jpaAnnotation to customize table structure

--- spring-data-jpa-dunky/src/main/java/com/dunky/springboot/entity/Product.java
+++ spring-data-jpa-dunky/src/main/java/com/dunky/springboot/entity/Product.java
@@ -1,17 +1,46 @@
 package com.dunky.springboot.entity;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Entity  // To specify this class as JPA entity
+@Table(
+        name="products",
+        schema = "ecommercespringdatajpa",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "sku_unique",
+                        columnNames = "sku"
+                )
+        }
+)
 public class Product {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_generator"
+     )
+
+    @SequenceGenerator(
+            name = "product_generator",
+            sequenceName = "product_sequence_name",
+            allocationSize=1
+    )
     private Long id;
+    @Column(name="sku", nullable = false)
     private String sku;
     private String name;
     private String description;
     private BigDecimal price;
     private boolean active;
     private String imageUrl;
+    @CreationTimestamp
     private LocalDateTime dateCreated;
+    @UpdateTimestamp
     private LocalDateTime lastUpdated;
 
     public Long getId() {
