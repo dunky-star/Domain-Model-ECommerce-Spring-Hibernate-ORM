30614ff44e96c56ce5e83b55288494041af829d6
dunky-star <dukygeoffrey39@gmail.com>
2022-09-06 18:44:34 +0300

    Lombok integration to remove boiler plate

--- spring-data-jpa-dunky/src/main/java/com/dunky/springboot/entity/Product.java
+++ spring-data-jpa-dunky/src/main/java/com/dunky/springboot/entity/Product.java
@@ -1,5 +1,9 @@
 package com.dunky.springboot.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -7,9 +11,14 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Entity  // To specify this class as JPA entity
+@Getter  // Lombok - for boiler plate removal.
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity  // To specify this class as JPA entity.
 @Table(
-        name="products",
+        name = "products",
         schema = "ecommercespringdatajpa",
         uniqueConstraints = {
                 @UniqueConstraint(
@@ -43,75 +52,4 @@ public class Product {
     @UpdateTimestamp
     private LocalDateTime lastUpdated;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSku() {
-        return sku;
-    }
-
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public LocalDateTime getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(LocalDateTime dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    public LocalDateTime getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(LocalDateTime lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
 }
