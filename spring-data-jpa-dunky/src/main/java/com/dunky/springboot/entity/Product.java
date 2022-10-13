dba0b3653a6782f40bca918b58bf8462c1f43e19
dunky-star <dukygeoffrey39@gmail.com>
2022-10-13 19:36:23 +0300

    Added test class to run and test the app

--- spring-data-jpa-dunky/src/main/java/com/dunky/springboot/entity/Product.java
+++ spring-data-jpa-dunky/src/main/java/com/dunky/springboot/entity/Product.java
@@ -1,9 +1,6 @@
 package com.dunky.springboot.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 
 @Entity  // To specify this class as JPA entity.
 @Table(
--- spring-data-jpa-dunky/src/test/java/com/dunky/springboot/repository/ProductRepositoryTest.java
+++ spring-data-jpa-dunky/src/test/java/com/dunky/springboot/repository/ProductRepositoryTest.java
@@ -1,10 +1,33 @@
 package com.dunky.springboot.repository;
 
+import com.dunky.springboot.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
 
 @SpringBootTest
 class ProductRepositoryTest {
+    @Autowired
+    private ProductRepository productRepository;
 
+    @Test
+    void saveMethod(){
+        // Create product
+        Product product = new Product();
+        product.setName("Sneaker shoe");
+        product.setDescription("Best sneaker in the market");
+        product.setSku("200ABC");
+        product.setPrice(new BigDecimal(300));
+        product.setActive(true);
+        product.setImageUrl("/home/sneaker.png");
+
+        // Save product
+        Product savedProduct = productRepository.save(product);
+
+        // Display product info
+        System.out.println(savedProduct.getId());
+        System.out.println(savedProduct.toString());
+    }
 }
