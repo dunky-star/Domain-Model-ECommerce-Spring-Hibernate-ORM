2d3ef5ad81f072456c5686677cd82c0f0ccfd061
dunky-star <dukygeoffrey39@gmail.com>
2022-10-17 12:27:30 +0300

    Spring Data JPA query methods

--- spring-data-jpa-dunky/src/main/java/com/dunky/springboot/repository/ProductRepository.java
+++ spring-data-jpa-dunky/src/main/java/com/dunky/springboot/repository/ProductRepository.java
@@ -3,13 +3,29 @@ package com.dunky.springboot.repository;
 import com.dunky.springboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     /**
-     * @param name
-     * @return
      * Returns the found product entry by using its name as search criteria.
      * If no product entry is found, this method returns null.
      */
     public Product findByName (String name);
+
+    /**
+     * Returns an Optional which contains the found product
+     * entry by using its id as search criteria.
+     * If no product entry is found, this method returns an empty Optional.
+     */
+    public Optional<Product> findById(Long id);
+
+
+    /**
+     * Returns the found list of product entries whose title or description is given
+     * as a method parameter.
+     * If no product entries is found, this method returns an empty list.
+     */
+    List<Product> findByNameOrDescription (String name, String Description);
 
 }
--- spring-data-jpa-dunky/src/test/java/com/dunky/springboot/repository/QueryMethodsTest.java
+++ spring-data-jpa-dunky/src/test/java/com/dunky/springboot/repository/QueryMethodsTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class QueryMethodsTest {
     @Autowired
@@ -16,4 +18,20 @@ public class QueryMethodsTest {
         System.out.println("Product ID: " + product.getId() + "\nProduct name: " + product.getName() +
                 "\nProduct description: " + product.getDescription());
     }
-}
+
+    @Test
+    void findByIdMethod(){
+        Product product = productRepository.findById(10L).get();
+        System.out.println("Product ID: " + product.getId() + "\nProduct name: " + product.getName() +
+                "\nProduct description: " + product.getDescription());
+    }
+
+    @Test
+    void findByNameOrDescriptionMethod(){
+        List<Product>  products = productRepository.findByNameOrDescription("Converse shoe - red",
+                "Best red Converse in the market");
+        products.forEach((p) -> {System.out.println("Product ID: " + p.getId() + "\nProduct name: " + p.getName() +
+                "\nProduct description: " + p.getDescription());});
+    }
+ }
+
