ab414dea6a8355b00b0978672d6bdeae350b9a1b
dunky-star <dukygeoffrey39@gmail.com>
2022-10-22 16:02:10 +0300

    Implementing the controller for search functionality using REST API

--- spring-data-jpa-dunky/src/main/java/com/dunky/springboot/controller/ProductController.java
+++ spring-data-jpa-dunky/src/main/java/com/dunky/springboot/controller/ProductController.java
@@ -1,2 +1,25 @@
-package com.dunky.springboot.controller;public class ProductController {
+package com.dunky.springboot.controller;
+
+import com.dunky.springboot.entity.Product;
+import com.dunky.springboot.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
 }
--- spring-data-jpa-dunky/src/main/java/com/dunky/springboot/service/impl/ProductServiceImpl.java
+++ spring-data-jpa-dunky/src/main/java/com/dunky/springboot/service/impl/ProductServiceImpl.java
@@ -1,15 +1,31 @@
 package com.dunky.springboot.service.impl;
 
 import com.dunky.springboot.entity.Product;
+import com.dunky.springboot.repository.ProductRepository;
 import com.dunky.springboot.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public List<Product> searchProducts(String query) {
-        return null;
+        List<Product> products = productRepository.searchProductsSQL(query);
+        return products;
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
     }
 }
