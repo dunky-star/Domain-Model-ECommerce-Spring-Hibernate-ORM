1745f7a9fff3a1411427bee346402d4a27272856
dunky-star <dukygeoffrey39@gmail.com>
2022-10-22 16:32:12 +0300

    Implementing the controller for search functionality using REST API

--- spring-data-jpa-dunky/src/main/java/com/dunky/springboot/controller/ProductController.java
+++ spring-data-jpa-dunky/src/main/java/com/dunky/springboot/controller/ProductController.java
@@ -3,19 +3,24 @@ package com.dunky.springboot.controller;
 import com.dunky.springboot.entity.Product;
 import com.dunky.springboot.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
-    private ProductService productService;
+    private final ProductService productService;
 
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam("query") String query){
+        return ResponseEntity.ok(productService.searchProducts(query));
     }
 
     @PostMapping
